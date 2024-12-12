2096407b2ee94311dd30ae409853ada0f3f38043
yoyo <yoyozheng97w@gmail.com>
2024-12-12 17:10:29 +0800

    update 121. Best Time to Buy and Sell Stock II

--- LeetCode_Python/0122_BestTimeToBuyAndSellStock_II.py
+++ LeetCode_Python/0122_BestTimeToBuyAndSellStock_II.py
@@ -17,6 +17,7 @@ Example 1:
     Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
     Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
     Total profit is 4 + 3 = 7.
+    
 Example 2:
     Input: prices = [1,2,3,4,5]
     Output: 4
@@ -32,7 +33,9 @@ Constraints:
     1 <= prices.length <= 3 * 104
     0 <= prices[i] <= 104
 """
-
+# Complexity:
+#     Time: O(n)
+#     Space: O(1)
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         n = len(prices)
