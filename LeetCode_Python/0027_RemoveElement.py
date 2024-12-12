7c97fb89291b43a0f3feb6cfebfadfc3934e8b63
yoyo <yoyozheng97w@gmail.com>
2024-12-12 16:56:19 +0800

    update 27. Remove Element

--- LeetCode_Python/0027_RemoveElement.py
+++ LeetCode_Python/0027_RemoveElement.py
@@ -1,17 +1,17 @@
 """
-27.Remove Element
+27. Remove Element
 
 Description:
-Given an integer array nums and an integer val, 
-remove all occurrences of val in nums in-place. The order of the elements may be changed. 
-Then return the number of elements in nums which are not equal to val.
+    Given an integer array nums and an integer val, 
+    remove all occurrences of val in nums in-place. The order of the elements may be changed. 
+    Then return the number of elements in nums which are not equal to val.
 
-Consider the number of elements in nums which are not equal to val be k, 
-to get accepted, you need to do the following things:
+    Consider the number of elements in nums which are not equal to val be k, 
+    to get accepted, you need to do the following things:
 
-Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
-The remaining elements of nums are not important as well as the size of nums.
-Return k.
+    Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+    The remaining elements of nums are not important as well as the size of nums.
+    Return k.
 
 Example 1:
     Input: nums = [3,2,2,3], val = 3
